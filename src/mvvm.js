bbf02186ee9cad139954116c7c8ca686dea23ea7
MargotSu <13178664072@163.com>
2021-06-08 23:40:17 +0800

    myVue

---
+++ src/mvvm.js
@@ -0,0 +1,33 @@
+const { observe } = require("./observer");
+var Compile=require('./compile')
+
+function MVVM(options) {
+    var self = this;
+    this.data = options.data;
+    this.methods = options.methods;
+
+    Object.keys(this.data).forEach(function (key) {
+        self.proxyKeys(key)
+    })
+
+    observe(this.data);
+    new Compile(options.el,this)
+}
+
+MVVM.prototype = {
+    proxyKeys: function (key) {
+        var self = this;
+        Object.defineProperty(this, key, {
+            enumerable: false,
+            configurable: true,
+            get: function getter() {
+                return self.data[key];
+            },
+            set: function setter(newVal) {
+                self.data[key]=newVal
+            }
+        })
+    }
+}
+
+module.exports=MVVM
